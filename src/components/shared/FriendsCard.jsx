4651dd717695b182815f4743fafe218aa8f0bb0a
tanjiyaJuthi <tanjiya098@gmail.com>
2026-05-05 12:07:07 +0600

    final environment variable

--- src/components/shared/FriendsCard.jsx
+++ src/components/shared/FriendsCard.jsx
@@ -1,55 +1,55 @@
-// import Image from "next/image";
-// import Link from "next/link";
+import Image from "next/image";
+import Link from "next/link";
 
-// const FriendsCard = ({friend}) => {
-//     const statusStyles = {
-//         overdue: "bg-[#EF4444]",
-//         'on track': "bg-[#244D3F]",
-//         'due soon': "bg-[#EFAD44]",
-//     };
+const FriendsCard = ({friend}) => {
+    const statusStyles = {
+        overdue: "bg-[#EF4444]",
+        'on track': "bg-[#244D3F]",
+        'due soon': "bg-[#EFAD44]",
+    };
+    
+    return (
+        <Link href={`friends/${friend.id}`} className="card bg-base-100 shadow-sm">
+            <figure className="px-6 pt-6">
+                <Image
+                    src={friend.picture}
+                    alt={friend.name}
+                    width={100}
+                    height={100}
+                    className="rounded-full object-cover"
+                />
+            </figure>
 
-//     return (
-//         <Link href={`friends/${friend.id}`} className="card bg-base-100 shadow-sm">
-//             <figure className="px-6 pt-6">
-//                 <Image
-//                     src={friend.picture}
-//                     alt={friend.name}
-//                     width={100}
-//                     height={100}
-//                     className="rounded-full object-cover"
-//                 />
-//             </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{friend.name}</h2>
 
-//             <div className="card-body items-center text-center">
-//                 <h2 className="card-title">{friend.name}</h2>
+                <p className="text-[#64748B] text-[10px]">62d ago</p>
 
-//                 <p className="text-[#64748B] text-[10px]">62d ago</p>
+                <div className="space-y-2">
+                    <p className="flex gap-2">
+                        {
+                            friend.tags.map((tag, index) => (
+                                <span 
+                                key={index}
+                                className="bg-[#CBFADB] rounded-full p-2 text-[10px] capitalize font-semibold text-[#244D3F]"
+                                >
+                                    {tag}
+                                </span>
+                            ))
+                        }   
+                    </p>
 
-//                 <div className="space-y-2">
-//                     <p className="flex gap-2">
-//                         {
-//                             friend.tags.map((tag, index) => (
-//                                 <span 
-//                                 key={index}
-//                                 className="bg-[#CBFADB] rounded-full p-2 text-[10px] capitalize font-semibold text-[#244D3F]"
-//                                 >
-//                                     {tag}
-//                                 </span>
-//                             ))
-//                         }   
-//                     </p>
+                    <p
+                        className={`rounded-full p-2 text-[10px] font-semibold text-white ${
+                            statusStyles[friend.status.toLowerCase()] || "bg-gray-400"
+                        }`}
+                    >
+                        {friend.status.charAt(0).toUpperCase() + friend.status.slice(1)}
+                    </p>
+                </div>                
+            </div>
+        </Link>
+    );
+};
 
-//                     <p
-//                         className={`rounded-full p-2 text-[10px] font-semibold text-white ${
-//                             statusStyles[friend.status.toLowerCase()] || "bg-gray-400"
-//                         }`}
-//                     >
-//                         {friend.status.charAt(0).toUpperCase() + friend.status.slice(1)}
-//                     </p>
-//                 </div>                
-//             </div>
-//         </Link>
-//     );
-// };
-
-// export default FriendsCard;
+export default FriendsCard;
