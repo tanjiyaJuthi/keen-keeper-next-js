8900ef73100ee9e969568f794d3fbcfe5a78080c
tanjiyaJuthi <tanjiya098@gmail.com>
2026-05-05 12:04:55 +0600

    added environment variable 2

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,22 +1,22 @@
 import FriendsCard from "./shared/FriendsCard";
 
 const Friends = async () => {
-    const res = await fetch(process.env.NEXT_APP_URL + "/friends.json");
-    const friends = await res.json();
+    // const res = await fetch(process.env.NEXT_APP_URL + "/friends.json");
+    // const friends = await res.json();
 
-    return (
-        <div className="pt-6 pb-20 border-t border-[#E9E9E9]">
-            <h3 className="text-xl font-semibold mb-5">Your Friends</h3>
+    // return (
+    //     <div className="pt-6 pb-20 border-t border-[#E9E9E9]">
+    //         <h3 className="text-xl font-semibold mb-5">Your Friends</h3>
 
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-10 justify-between">
-                {
-                    friends.map(friend =>
-                        <FriendsCard key={friend.id} friend={friend} />
-                    )
-                }
-            </div>
-        </div>
-    );
+    //         <div className="grid grid-cols-2 lg:grid-cols-4 gap-10 justify-between">
+    //             {
+    //                 friends.map(friend =>
+    //                     <FriendsCard key={friend.id} friend={friend} />
+    //                 )
+    //             }
+    //         </div>
+    //     </div>
+    // );
 };
 
 export default Friends;
--- src/components/shared/FriendsCard.jsx
+++ src/components/shared/FriendsCard.jsx
@@ -1,55 +1,55 @@
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 
-const FriendsCard = ({friend}) => {
-    const statusStyles = {
-        overdue: "bg-[#EF4444]",
-        'on track': "bg-[#244D3F]",
-        'due soon': "bg-[#EFAD44]",
-    };
-    
-    return (
-        <Link href={`friends/${friend.id}`} className="card bg-base-100 shadow-sm">
-            <figure className="px-6 pt-6">
-                <Image
-                    src={friend.picture}
-                    alt={friend.name}
-                    width={100}
-                    height={100}
-                    className="rounded-full object-cover"
-                />
-            </figure>
+// const FriendsCard = ({friend}) => {
+//     const statusStyles = {
+//         overdue: "bg-[#EF4444]",
+//         'on track': "bg-[#244D3F]",
+//         'due soon': "bg-[#EFAD44]",
+//     };
 
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">{friend.name}</h2>
+//     return (
+//         <Link href={`friends/${friend.id}`} className="card bg-base-100 shadow-sm">
+//             <figure className="px-6 pt-6">
+//                 <Image
+//                     src={friend.picture}
+//                     alt={friend.name}
+//                     width={100}
+//                     height={100}
+//                     className="rounded-full object-cover"
+//                 />
+//             </figure>
 
-                <p className="text-[#64748B] text-[10px]">62d ago</p>
+//             <div className="card-body items-center text-center">
+//                 <h2 className="card-title">{friend.name}</h2>
 
-                <div className="space-y-2">
-                    <p className="flex gap-2">
-                        {
-                            friend.tags.map((tag, index) => (
-                                <span 
-                                key={index}
-                                className="bg-[#CBFADB] rounded-full p-2 text-[10px] capitalize font-semibold text-[#244D3F]"
-                                >
-                                    {tag}
-                                </span>
-                            ))
-                        }   
-                    </p>
+//                 <p className="text-[#64748B] text-[10px]">62d ago</p>
 
-                    <p
-                        className={`rounded-full p-2 text-[10px] font-semibold text-white ${
-                            statusStyles[friend.status.toLowerCase()] || "bg-gray-400"
-                        }`}
-                    >
-                        {friend.status.charAt(0).toUpperCase() + friend.status.slice(1)}
-                    </p>
-                </div>                
-            </div>
-        </Link>
-    );
-};
+//                 <div className="space-y-2">
+//                     <p className="flex gap-2">
+//                         {
+//                             friend.tags.map((tag, index) => (
+//                                 <span 
+//                                 key={index}
+//                                 className="bg-[#CBFADB] rounded-full p-2 text-[10px] capitalize font-semibold text-[#244D3F]"
+//                                 >
+//                                     {tag}
+//                                 </span>
+//                             ))
+//                         }   
+//                     </p>
 
-export default FriendsCard;
+//                     <p
+//                         className={`rounded-full p-2 text-[10px] font-semibold text-white ${
+//                             statusStyles[friend.status.toLowerCase()] || "bg-gray-400"
+//                         }`}
+//                     >
+//                         {friend.status.charAt(0).toUpperCase() + friend.status.slice(1)}
+//                     </p>
+//                 </div>                
+//             </div>
+//         </Link>
+//     );
+// };
+
+// export default FriendsCard;
